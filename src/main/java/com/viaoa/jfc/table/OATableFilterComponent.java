4b5fd88caf257f2dd3c6a6e5a6c04ba79434183b
Vince Via <vince@viaoa.com>
2019-04-09 10:18:35 -0400

    javadoc

--- src/main/java/com/viaoa/jfc/table/OATableFilterComponent.java
+++
@@ -1,9 +0,0 @@
-package com.viaoa.jfc.table;
-
-import com.viaoa.util.OAFilter;
-
-public interface OATableFilterComponent extends OATableComponent, OAFilter {
-    void reset();
-    boolean isBeingUsed();
-
-}
